2574be0684079555def4c2a647415d6feaf2fe8f
derrin chong <dchong@revacomm.com>
2019-08-16 18:31:07 +0000

    Initial version.  First application just to get used to the build process with github and jenkins (eventually).

---
+++ hello.py
@@ -0,0 +1,22 @@
+from flask import Flask
+from markupsafe import Markup, escape
+app = Flask(__name__)
+
+@app.route("/")
+def index():
+    return("Index!\n")
+
+@app.route("/hello")
+def hello():
+    return("Hello world!\n")
+
+@app.route("/members")
+def members():
+    return("Members, hello!\n")
+
+@app.route("/members/<memberName>")
+def getMember(memberName):
+    return("Hello %s\n" % escape(memberName))
+
+if(__name__ == "__main__"):
+    app.run()
